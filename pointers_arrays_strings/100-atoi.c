6cab8e4eb768566122be43467aa762a38ef11f06
razan <razan_8801@icloud.com>
2025-02-26 10:37:55 +0000

    Fix spacing issue for Betty style

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _atoi - converts a string to an integer
+* @s: the string to be converted
+*
+* Return: the integer value of the converted string
+*/
+int _atoi(char *s)
+{
+int i = 0, sign = 1, result = 0;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+sign = sign * -1;
+i++;
+}
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+{
+result = result * 10 + (s[i] - '0');
+i++;
+}
+return (result *sign);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif /* MAIN_H */
