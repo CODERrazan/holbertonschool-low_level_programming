67ba1c61bd0b3cffbc4e591e7585ac7ae54085e0
razan <razan_8801@icloud.com>
2025-02-28 19:27:19 +0000

    Add cap_string function to capitalize all words in a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,23 @@
-#include <string.h>
 #include "main.h"
+/**
+* is_separator - Checks if a character is a separator.
+* @c: The character to check.
+*
+* Return: 1 if the character is a separator, 0 otherwise.
+*/
+int is_separator(char c)
+{
+char separators[] = " \t\n,;.!?\"(){}";
+int i;
+for (i = 0; separators[i] != '\0'; i++)
+{
+if (c == separators[i])
+{
+return (1);
+}
+}
+return (0);
+}
 /**
 * cap_string - Capitalizes all words of a string.
 * @str: The string to be modified.
@@ -9,15 +27,15 @@
 char *cap_string(char *str)
 {
 int i = 0;
-int is_separator;
-char separators[] = " \t\n,;.!?\"(){}";
 while (str[i] != '\0')
 {
-is_separator = 0;
-if (i == 0 || strchr(separators, str[i - 1]) != NULL)
-is_separator = 1;
-if (is_separator && str[i] >= 'a' && str[i] <= 'z')
+if (i == 0 || is_separator(str[i - 1]))
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
 str[i] = str[i] - 32;
+}
+}
 i++;
 }
 return (str);
