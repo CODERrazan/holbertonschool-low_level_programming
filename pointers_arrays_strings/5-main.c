c5c6cbb406c8656967c8f2259d97f783fe515f7d
razan <razan_8801@icloud.com>
2025-03-05 10:10:52 +0000

    Add _strstr function to locate a substring

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - check the code
+* main - Check the code.
 *
 * Return: Always 0.
 */
 int main(void)
 {
-char str[] = "Look up!\n";
-char *ptr;
-ptr = string_toupper(str);
-printf("%s", ptr);
-printf("%s", str);
+char *s = "hello, world";
+char *f = "world";
+char *t;
+t = _strstr(s, f);
+printf("%s\n", t);
 return (0);
 }
