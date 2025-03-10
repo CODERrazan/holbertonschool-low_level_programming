af1f41352162ea3dac98d8f4e399037e59608cd6
razan <razan_8801@icloud.com>
2025-03-10 23:13:27 +0000

    Implement alloc_grid function with memory management and tests

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
 int _putchar(char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
 #endif /* MAIN_H */
