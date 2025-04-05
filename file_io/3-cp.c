7b487eb21c8d00ca6a7a4fb4428a6949b1ca77ff
razan <razan_8801@icloud.com>
2025-04-05 14:10:37 +0000

    read

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -34,7 +34,7 @@ fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 error_exit(98, "Error: Can't read from file", argv[1]);
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 if (fd_to == -1)
 error_exit(99, "Error: Can't write to", argv[2]);
 while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
