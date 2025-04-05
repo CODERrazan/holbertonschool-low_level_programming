91b9b1d39b7ce7c10b2088dc14c5fc09c254d306
razan <razn_8801@icloud.com>
2025-04-05 17:00:28 +0000

    mm3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
-* error_exit - Prints an error message and exits with the specified code.
+* error_exit - Prints an error message and exits the program.
 * @code: The exit code.
-* @msg: The error message to print.
-* @file: The filename to include in the error message, if applicable.
+* @msg: The error message.
+* @file: The file name to include in the error message.
 */
 void error_exit(int code, const char *msg, const char *file)
 {
@@ -37,14 +37,17 @@ fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd_to == -1)
 error_exit(99, "Error: Can't write to", argv[2]);
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+while (1)
 {
+bytes_read = read(fd_from, buffer, 1024);
+if (bytes_read == -1)
+error_exit(98, "Error: Can't read from file", argv[1]);
+if (bytes_read == 0) /* End of file */
+break;
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written != bytes_read)
 error_exit(99, "Error: Can't write to", argv[2]);
 }
-if (bytes_read == -1)
-error_exit(98, "Error: Can't read from file", argv[1]);
 if (close(fd_from) == -1)
 error_exit(100, "Error: Can't close fd", argv[1]);
 if (close(fd_to) == -1)
